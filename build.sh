792f1f2e3a736af95e866b6677c345a00f215b1a
Piotr Persona <persona.piotr@gmail.com>
2024-01-09 11:14:47 +0100

    cmd variable
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@ PROJECT_PATH=$( pwd )
 go build -ldflags \
     "-X github.com/piotrpersona/go-build-flags/pkg/cmd.Variable=VALUE -X main.Variable=MAIN_VALUE" \
     -o bin_path $PROJECT_PATH
+
+go build -ldflags \
+    "-X main.cmdVar=CMD" \
+    -o cmd_path $PROJECT_PATH/cmd
---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var cmdVar string
+
+func main() {
+	fmt.Println("CMD VAR:", cmdVar)
+}
